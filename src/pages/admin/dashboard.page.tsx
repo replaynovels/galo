8f22569bd8f69299c549610398022441bcf5256a
Drew <drewsup123c@gmail.com>
2022-10-29 11:31:28 -0600

    boilerplate level editor

--- src/pages/admin/dashboard.page.tsx
+++ src/pages/admin/dashboard.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ROUTE_CLASS } from '../../contants/commonClasses';
+import LevelEditor from './levelEditor.page';
 
 const AdminDashboard = () => {
     return(
         <div className={ROUTE_CLASS}>
-
+            <h1>Example Flowspace</h1>
+            <LevelEditor />
         </div>
     )
 }
