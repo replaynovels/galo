a0b055a751b040e620a400f5ffcc094b9df942f8
Drew <drewsup123c@gmail.com>
2022-10-29 15:20:07 -0600

    added firestore

--- src/pages/admin/levelEditor.page.tsx
+++ src/pages/admin/levelEditor.page.tsx
@@ -7,33 +7,165 @@ import ReactFlow, {
     useEdgesState,
     addEdge,
 } from 'reactflow';
+import MultiNode from "../../components/LevelEditor/multiNode.component";
 import 'reactflow/dist/style.css';
+import { MarkerType, Position } from 'reactflow';
 
-const initialNodes = [
-    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+export const initialNodes: any = [
+  {
+    id: '1',
+    type: 'input',
+    data: {
+      label: 'Input Node',
+    },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: '2',
+    data: {
+      label: 'Default Node',
+    },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    type: 'output',
+    data: {
+      label: 'Output Node',
+    },
+    position: { x: 400, y: 100 },
+  },
+  {
+    id: '4',
+    type: 'custom',
+    position: { x: 100, y: 200 },
+    data: {
+      selects: {
+        'handle-0': 'smoothstep',
+        'handle-1': 'smoothstep',
+      },
+    },
+  },
+  {
+    id: '5',
+    type: 'output',
+    data: {
+      label: 'custom style',
+    },
+    className: 'circle',
+    style: {
+      background: '#2B6CB0',
+      color: 'white',
+    },
+    position: { x: 400, y: 200 },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+  {
+    id: '6',
+    type: 'output',
+    style: {
+      background: '#63B3ED',
+      color: 'white',
+      width: 100,
+    },
+    data: {
+      label: 'Node',
+    },
+    position: { x: 400, y: 325 },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+  {
+    id: '7',
+    type: 'default',
+    className: 'annotation',
+    data: {
+      label: (
+        <>
+          On the bottom left you see the <strong>Controls</strong> and the bottom right the{' '}
+          <strong>MiniMap</strong>. This is also just a node 🥳
+        </>
+      ),
+    },
+    draggable: false,
+    selectable: false,
+    position: { x: 150, y: 400 },
+  },
 ];
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+export const initialEdges: any = [
+  { id: 'e1-2', source: '1', target: '2', label: 'this is an edge label' },
+  { id: 'e1-3', source: '1', target: '3', animated: true },
+  {
+    id: 'e4-5',
+    source: '4',
+    target: '5',
+    type: 'smoothstep',
+    sourceHandle: 'handle-0',
+    data: {
+      selectIndex: 0,
+    },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'e4-6',
+    source: '4',
+    target: '6',
+    type: 'smoothstep',
+    sourceHandle: 'handle-1',
+    data: {
+      selectIndex: 1,
+    },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+];
+
+const nodeTypes = {
+    custom: MultiNode,
+};
+
+const minimapStyle = {
+    height: 120,
+};
+
+const onInit = (reactFlowInstance: any) => console.log('flow loaded:', reactFlowInstance);
 
 const LevelEditor = () => {
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-    const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+    const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), []);
+  
+    // we are using a bit of a shortcut here to adjust the edge type
+    // this could also be done with a custom edge for example
+    const edgesWithUpdatedTypes = edges.map((edge) => {
+        if (edge.sourceHandle && nodes) {
+            const edgeType = nodes.find((node) => node.type === 'custom')?.data.selects[edge.sourceHandle];
+            edge.type = edgeType;
+        }
+        return edge;
+    });
 
     return (
         <div style={{width: "100%", height: "100vh"}}>
             <ReactFlow
                 nodes={nodes}
-                edges={edges}
+                edges={edgesWithUpdatedTypes}
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
+                onInit={onInit}
+                fitView
+                attributionPosition="top-right"
+                nodeTypes={nodeTypes}
             >
-                <MiniMap />
+                <MiniMap style={minimapStyle} />
                 <Controls />
-                <Background />
+                <Background color="#aaa" gap={16} />
             </ReactFlow>
         </div>
     );
